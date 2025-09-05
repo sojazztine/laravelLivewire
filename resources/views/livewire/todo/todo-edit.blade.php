b94a721e1b17d62bca78c2743f7a56a0831b37dc
AAPjazztine <jazztine.so@aap.org.ph>
2025-09-05 09:17:25 +0800

    update todo-edit form to use 'submit' action and change button label to 'Create'

--- resources/views/livewire/todo/todo-edit.blade.php
+++ resources/views/livewire/todo/todo-edit.blade.php
@@ -1,3 +1,34 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <x-header/>
+    <x-card>
+        {{-- <x-breadcrumbs :items="[
+        ['label' => 'To do list', 'url' => route('todo.index')],
+        ['label' => 'Create', 'url' => route('todo.create')]
+        ]" /> --}}
+        <x-breadcrumbs/>
+
+
+    <form action="" wire:submit="submit">
+
+        <fieldset class="fieldset">
+            <legend class="fieldset-legend">Title</legend>
+            <input type="text" class="input" placeholder="Type here" wire:model="title"  value=""/>
+            <em class="label">Optional</em>
+        </fieldset>
+
+        <fieldset class="fieldset">
+            <legend class="fieldset-legend">Description</legend>
+            <input type="text" class="input  " placeholder="Type here" wire:model="description" />
+            <em class="label">Optional</em>
+        </fieldset>
+
+        <div class="flex justify-end">
+            <flux:button type="submit" flux:button variant="primary" color="sky" class="cursor-pointer">Create</flux:button>
+        </div>
+
+    </form>
+
+
+
+    </x-card>
 </div>
