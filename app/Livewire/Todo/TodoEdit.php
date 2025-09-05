f608e9fe73f2f5ba5893ec04ce4af1a0862bde21
AAPjazztine <jazztine.so@aap.org.ph>
2025-09-05 09:17:53 +0800

    implement todo update functionality with validation and success message

--- app/Livewire/Todo/TodoEdit.php
+++ app/Livewire/Todo/TodoEdit.php
@@ -2,10 +2,39 @@
 
 namespace App\Livewire\Todo;
 
+use App\Models\Todo;
 use Livewire\Component;
 
 class TodoEdit extends Component
 {
+    public $title, $description, $todoId;
+
+    public function mount($id)
+    {
+        $todo = Todo::find($id);
+        $this->todoId = $id;
+        $this->title = $todo->title;
+        $this->description = $todo->description;
+    }
+
+    public function update(){
+
+        $this->validate([
+            'title' => 'required',
+            'description'=> 'required'
+        ]);
+
+        $todo = Todo::findOrFail($this->todoId);
+
+        $todo->update([
+            'title' => $this->title,
+            'description' => $this->description,
+        ]);
+        session()->flash('success', 'Todo Updated Successfully');
+        return redirect()->to('/todo');
+
+    }
+
     public function render()
     {
         return view('livewire.todo.todo-edit');
